4e62137322330ac1e558922a066a461df0a23feb
Hepheir <hepheir@gmail.com>
2020-10-18 03:43:47 +0900

    Remove lowered cache

--- scripts/python/simpleicons/org/from_cache.py
+++ scripts/python/simpleicons/org/from_cache.py
@@ -3,7 +3,7 @@ from . import CACHED_COLOR_LOWER
 from ...shields.io import makeurl
 
 def from_cache(name, link, style='flat-square', logoColorWhite=True):
-    color = CACHED_COLOR_LOWER[name.lower()]
+    color = CACHED_COLOR_LOWER[name]
     options = [('style', style),
                ('labelColor', color),
                ('logo', name),
