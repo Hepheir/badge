39ff91e738014d1319fd07af898a460eb0147e5f
Hepheir <hepheir@gmail.com>
2020-10-18 01:20:06 +0900

    Fix import syntax & Add annotation

--- scripts/shields/io/makeurl.py
+++ scripts/shields/io/makeurl.py
@@ -1,10 +1,10 @@
 import urllib.parse
 
-from . import urlencode
+from .urlencode import urlencode
 
 BASE_URL = 'https://img.shields.io/badge/'
 
-def makeurl(label='', message='', color='', options=[tuple, ...]):
+def makeurl(label:str='', message:str='', color:str='', options:[tuple, ...]=[]):
     sub_url = '-'.join(map(urlencode,(label, message, color)))
     query = urllib.parse.urlencode(options)
     return BASE_URL + sub_url + '?' + query
