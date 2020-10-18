3470299899df73b8b84853c54c232c3712e565ec
Hepheir <hepheir@gmail.com>
2020-10-18 22:01:38 +0900

    Update quick_badge.py

--- scripts/quick_badge.py
+++ scripts/quick_badge.py
@@ -1,13 +1,19 @@
-from python.download import save_at
+import os
+
+from python.core.download import save_at
 
 from python.acmicpc.net import problem
 from python.solved.ac import difficulty, all_dificulties
 from python.github.com import user, repository
 from python.simpleicons.org import from_cache
 
+from python.simpleicons.org import ARCHIVE
+
 # Example Code
-url = kickstart_from_cache('Round F 2020')
-file_path = 'svg/competitions.withgoogle.com/kick-start/round-f-2020.svg'
+name, link = 'Gmail', 'https://mail.google.com/'
+
+url = from_cache(name, link)
+file_path = os.path.join(ARCHIVE, f'{name.lower()}.svg')
 
 save_at(url, file_path)
 
