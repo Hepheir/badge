d154da8c574a81d1781ccb0fb321ec35d5d09e36
Hepheir <hepheir@gmail.com>
2020-10-19 09:23:33 +0900

    Update quick_badge.py

--- scripts/quick_badge.py
+++ scripts/quick_badge.py
@@ -5,15 +5,15 @@ from python.core.download import save_at
 from python.acmicpc.net import problem
 from python.solved.ac import difficulty, all_dificulties
 from python.github.com import user, repository
-from python.simpleicons.org import from_cache
+from python.simpleicons.org import from_cache, from_cache_lower
 
 from python.simpleicons.org import ARCHIVE
 
 # Example Code
-name, link = 'Gmail', 'https://mail.google.com/'
+name, link = 'github', 'https://hepheir.github.io/'
 
-url = from_cache(name, link)
-file_path = os.path.join(ARCHIVE, f'{name.lower()}.svg')
+url = from_cache_lower(name, link, 'Dev Blog')
+file_path = os.path.join('svg/util/blog', f'hepheir.svg')
 
 save_at(url, file_path)
 
