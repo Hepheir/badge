ba26802790de1afd8dc281099cfd02eacec6d44e
Hepheir <hepheir@gmail.com>
2020-10-19 09:23:10 +0900

    Create from_cache_lower() @ python.simpleicons.org

--- scripts/python/simpleicons/org/__init__.py
+++ scripts/python/simpleicons/org/__init__.py
@@ -4,3 +4,4 @@ BASE_URL = 'https://simpleicons.org/'
 
 from .cached import CACHED_COLOR
 from .from_cache import from_cache
+from .from_cache_lower import from_cache_lower
--- scripts/python/simpleicons/org/from_cache.py
+++ scripts/python/simpleicons/org/from_cache.py
@@ -2,11 +2,13 @@ from . import CACHED_COLOR
 
 from ...shields.io import makeurl
 
-def from_cache(name, link, style='flat-square', logoColorWhite=True):
-    color = CACHED_COLOR[name]
+def from_cache(logo:str, link:str, name:str='', style='flat-square', logoColorWhite=True):
+    if not name:
+        name = logo
+    color = CACHED_COLOR[logo]
     options = [('style', style),
                ('labelColor', color),
-               ('logo', name),
+               ('logo', logo),
                ('link', link),
                ('logoColor', 'FAFAFA')]
     return makeurl('', name, color, options)
