9db065a9e79aaf3223bec71645cdda0833343dbe
Hepheir <hepheir@gmail.com>
2020-11-09 07:41:11 -0800

    Minor Update

--- scripts/quick_git_repo.py
+++ scripts/quick_git_repo.py
@@ -3,10 +3,13 @@ import os
 from python.core.download import save_at
 from python.github.com import user, repository, ARCHIVE
 
-owner = 'DGU-DAI-Lab'
-repo = 'giwa-damage-detector-demo'
+owner = 'Hepheir'
+repo = 'hepheir.github.io'
 
-url = repository(owner, repo)
-file_path = os.path.join(ARCHIVE, owner, f'{repo}.svg')
+# url = repository(owner, repo)
+# file_path = os.path.join(ARCHIVE, owner, f'{repo}.svg')
+
+url = user(owner)
+file_path = os.path.join(ARCHIVE, f'{owner}.svg')
 
 save_at(url, file_path)
