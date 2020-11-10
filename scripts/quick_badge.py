41087f9ecbff517cddfadd351dec40797addc391
Hepheir <hepheir@gmail.com>
2020-11-10 11:50:53 +0900

    Minor Update

--- scripts/quick_badge.py
+++ scripts/quick_badge.py
@@ -9,11 +9,13 @@ from python.simpleicons.org import from_cache, from_cache_lower
 
 from python.simpleicons.org import ARCHIVE
 
-# Example Code
-name, link = 'github', 'https://hepheir.github.io/'
+ROOT = os.path.dirname(os.path.dirname(__file__))
 
-url = from_cache_lower(name, link, 'Dev Blog')
-file_path = os.path.join('svg/util/blog', f'hepheir.svg')
+# Example Code
+name, link = 'react', 'https://ko.reactjs.org/'
+
+url = from_cache_lower(name, link)
+file_path = os.path.join(ROOT, ARCHIVE, name+'.svg')
 
 save_at(url, file_path)
 
